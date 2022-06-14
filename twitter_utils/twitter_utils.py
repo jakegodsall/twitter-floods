d6a71504c6b5aea3beb4845ef9cecd58faf5e5db
jakegodsall <jake.edward.godsall@gmail.com>
2022-06-14 17:16:27 +0100

    merged Processor.extract_geo() into Processor.extract_coords()
    Refactored Processor.extract_coords()

--- twitter_utils/twitter_utils.py
+++ twitter_utils/twitter_utils.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 
+
 class Loader:
     def __init__(self, tweet_dir, places_dir):
         """Loads the tweet and places files."""
@@ -17,6 +18,7 @@ class Loader:
 
         return tweet_df, places_df
 
+
 class Processor:
     def __init__(self, tweets_df, places_df):
         self.tweets_df = tweets_df
@@ -25,23 +27,28 @@ class Processor:
     def filter_geo(self):
         self.tweets_df = self.tweets_df[self.tweets_df.geo.notnull()]
 
-
-    def extract_geo(self):
+    def extract_coords(self):
+        # extract dictionary in geo feature
         expanded_geo = self.tweets_df.geo.apply(lambda s: pd.Series(s))
+        # concatenate the new df to the original df
         self.tweets_df = pd.concat([self.tweets_df, expanded_geo], axis=1)
 
-    def extract_coords(self):
+        # flatten dictionary in coordinates feature
         expanded_coords = self.tweets_df.coordinates.apply(lambda s: pd.Series(s))
-        expanded_coords.columns = [0, 'type', 'actual_coordinates']
+        # rename new features from dictionary
+        expanded_coords.columns = [0, 'type', 'gt_coordinates']
+        # concatenate the new df to the original df
         self.tweets_df = pd.concat([self.tweets_df, expanded_coords], axis=1)
 
-        lonlat = self.tweets_df.actual_coordinates.apply(lambda s: pd.Series(s))
-        lonlat.columns = ['longitude', 'latitude']
+        # flatten longitude and latitude
+        lonlat = self.tweets_df.gt_coordinates.apply(lambda s: pd.Series(s))
+        # rename new features from dictionary
+        lonlat.columns = ['gt_longitude', 'gt_latitude']
+        # concatenate the new df to the original df
         self.tweets_df = pd.concat([self.tweets_df, lonlat], axis=1)
 
     def extract_features(self):
         self.filter_geo()
-        self.extract_geo()
         self.extract_coords()
 
         return self.tweets_df
