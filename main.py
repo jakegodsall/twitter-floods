d6158856deb52a10846c2aed59ccdf26fcb3c7b1
Jake Godsall <jake.edward.godsall@gmail.com>
2022-07-18 14:53:45 +0100

    Added filedialog's for browsing for labelled tweets, places and plot directory

--- main.py
+++ main.py
@@ -56,6 +56,37 @@
 import tkinter as tk
 from tkinter import ttk
 
+from tkinter import filedialog
+
+def browse_labelled_tweets():
+    filename = filedialog.askopenfilename(initialdir="./data/StormFranklin/",
+                                          title="Select file",
+                                          filetypes=(("JSON Lines",
+                                                     "*.jsonl"),
+                                                     ("All files",
+                                                      "*.*")))
+
+    just_file = filename.split("/")[-1]
+    labelled_tweets_dir_browse["text"] = just_file
+
+def browse_places():
+    filename = filedialog.askopenfilename(initialdir="./data/StormFranklin/",
+                                          title="Select file",
+                                          filetypes=(("JSON Lines",
+                                                      "*.jsonl"),
+                                                     ("All files",
+                                                      "*.*")))
+
+    just_file = filename.split("/")[-1]
+    places_dir_browse["text"] = just_file
+
+def browse_plots():
+    filename = filedialog.askdirectory(initialdir="./plots",
+                                       title="Select directory")
+
+    just_file = filename.split("/")[-1]
+    save_dir_browse["text"] = just_file
+
 window = tk.Tk()
 window.geometry("1000x600")
 window.resizable(False, False)
@@ -81,23 +112,17 @@ dir_frame.grid(row=0, column=0)
 
 # labelled tweets
 ttk.Label(dir_frame, text="Labelled Tweets: ").grid(row=1, column=0)
-labelled_tweets_dir = tk.Text(dir_frame, height=1)
-labelled_tweets_dir.grid(row=1, column=1)
-labelled_tweets_dir_browse = ttk.Button(dir_frame)
+labelled_tweets_dir_browse = ttk.Button(dir_frame, command=browse_labelled_tweets)
 labelled_tweets_dir_browse.grid(row=1, column=2)
 
 # places
 ttk.Label(dir_frame, text="Places: ").grid(row=2, column=0)
-places_dir = tk.Text(dir_frame, height=1)
-places_dir.grid(row=2, column=1)
-places_dir_browse = ttk.Button(dir_frame)
+places_dir_browse = ttk.Button(dir_frame, command=browse_places)
 places_dir_browse.grid(row=2, column=2)
 
 # plots
 ttk.Label(dir_frame, text="Save: ").grid(row=3, column=0)
-save_dir = tk.Text(dir_frame, height=1)
-save_dir.grid(row=3, column=1)
-save_dir_browse = ttk.Button(dir_frame)
+save_dir_browse = ttk.Button(dir_frame, command=browse_plots)
 save_dir_browse.grid(row=3, column=2)
 
 ttk.Separator(options_frame, orient="horizontal").grid(row=4, ipadx=200, ipady=10)
@@ -106,8 +131,6 @@ ttk.Separator(options_frame, orient="horizontal").grid(row=4, ipadx=200, ipady=1
 
 meta_frame = ttk.Frame(options_frame)
 meta_frame.grid(row=1, column=0)
-
-
 
 
 ttk.Separator(options_frame, orient="horizontal").grid(row=4, ipadx=200, ipady=10)
