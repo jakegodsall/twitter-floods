3ec6e027c5ce189f2eaf33d7ec3aeb02ca5a755f
Jake Godsall <jake.edward.godsall@gmail.com>
2022-07-20 17:38:19 +0100

    All options elements are in place

--- main.py
+++ main.py
@@ -75,19 +75,19 @@ class DataFrame(ttk.Frame):
         self.submit_button = ttk.Button(self, text="Submit")
 
         # data frame layout
-        self.data_label.grid(row=0, column=0, columnspan=2)
+        self.data_label.grid(row=0, column=0, columnspan=2, pady=10)
         self.labelled_tweets_label.grid(row=1, column=0)
         self.labelled_tweets_button.grid(row=1, column=1)
         self.places_label.grid(row=2, column=0)
         self.places_button.grid(row=2, column=1)
         self.plots_label.grid(row=3, column=0)
         self.plots_button.grid(row=3, column=1)
-        self.submit_button.grid(row=4, column=0, columnspan=2)
+        self.submit_button.grid(row=4, column=0, columnspan=2, pady=10)
 
 
 class MetaFrame(ttk.Frame):
     def __init__(self, main, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(main, *args, **kwargs)
         # meta frame components
         self.meta_label = ttk.Label(self, text="Tweet Metadata")
         self.meta_textbox = ttk.Entry(self)
@@ -95,6 +95,7 @@ class MetaFrame(ttk.Frame):
         # meta frame layout
         self.meta_label.grid(row=0, column=0)
         self.meta_textbox.grid(row=1, column=0)
+
 
 class SaveFrame(ttk.Frame):
     def __init__(self, main, *args, **kwargs):
@@ -105,41 +106,55 @@ class SaveFrame(ttk.Frame):
 
         # save frame layout
         self.plot_button.grid(row=0, column=0)
-        self.save_button.grid(row=1, column=0)
+        self.save_button.grid(row=0, column=1)
+
 
 class OptionsFrame(ttk.Frame):
     def __init__(self, main, *args, **kwargs):
         super().__init__(main, *args, **kwargs)
         # options frame components
         self.data_frame = DataFrame(self)
+        self.horiz1 = ttk.Separator(self, orient="horizontal")
         self.meta_frame = MetaFrame(self)
+        self.horiz2 = ttk.Separator(self, orient="horizontal")
         self.save_frame = SaveFrame(self)
 
         # options frame layout
         self.data_frame.grid(row=0, column=0)
-        self.meta_frame.grid(row=1, column=0)
-        self.save_frame.grid(row=2, column=0)
+        self.horiz1.grid(row=1, column=0, sticky="ew", pady=30)
+        self.meta_frame.grid(row=2, column=0)
+        self.horiz2.grid(row=3, column=0, sticky="ew", pady=30)
+        self.save_frame.grid(row=4, column=0, sticky="s")
 
-class MainWindow:
-    def __init__(self, main):
-        self.main = main
-        self.main.title("Twitter Floods")
-        # frames and components
-        self.options_frame = OptionsFrame(self.main)
-        self.plot_frame = tk.Frame(self.main, bg="green")
 
+class PlotFrame(ttk.Frame):
+    def __init__(self, main, *args, **kwargs):
+        super().__init__(main, *args, **kwargs)
+
+
+class MainWindow(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("Twitter Floods")
+        self.geometry("800x600")
+        self.resizable(False, False)
+
+        # configuration
+        self.columnconfigure(index=0, weight=1)
+        self.columnconfigure(index=1, weight=2)
+
+        # main frames
+        self.options_frame = OptionsFrame(self, width=400, height=600, borderwidth=1)
+        self.vert = ttk.Separator(self, orient="vertical")
+        self.plot_frame = PlotFrame(self, width=400, height=600, borderwidth=1)
         # main layout
+
         self.options_frame.grid(row=0, column=0)
-        self.plot_frame.grid(row=0, column=1)
-
-
-
+        self.vert.grid(row=0, column=1, sticky="ns")
+        self.plot_frame.grid(row=0, column=2)
 
 def main():
-    root = tk.Tk()
-    root.geometry("800x600")
-    root.resizable(False, False)
-    MainWindow(root)
+    root = MainWindow()
     root.mainloop()
 
 
