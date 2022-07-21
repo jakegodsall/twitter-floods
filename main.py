33634f9371fa182d84b57c86d2b9341211032445
Jake Godsall <jake.edward.godsall@gmail.com>
2022-07-21 13:08:10 +0100

    Added filedialogs for tweets, places and plots

--- main.py
+++ main.py
@@ -61,19 +61,27 @@ from tkinter import filedialog
 class DataFrame(ttk.Frame):
     def __init__(self, main, *args, **kwargs):
         super().__init__(main, *args, **kwargs)
+        # paths
+        self.data_loaded = False
+        self.tweets_dir = None
+        self.places_dir = None
+        self.plots_dir = None
+
         # data frame elements
         self.data_label = ttk.Label(self, text="Add Data:")
 
         self.labelled_tweets_label = ttk.Label(self, text="Labelled-Tweets: ")
-        self.labelled_tweets_button = ttk.Button(self, text="Browse", command=self.load_labelled)
+        self.labelled_tweets_button = ttk.Button(self, text="Browse", command=self.get_labelled_tweets_dir)
 
         self.places_label = ttk.Label(self, text="Places: ")
-        self.places_button = ttk.Button(self, text="Browse", command=self.load_places)
+        self.places_button = ttk.Button(self, text="Browse", command=self.get_places_dir)
 
         self.plots_label = ttk.Label(self, text="Plots Path: ")
-        self.plots_button = ttk.Button(self, text="Browse", command=self.ask_dir)
+        self.plots_button = ttk.Button(self, text="Browse", command=self.get_plots_dir)
 
-        self.submit_button = ttk.Button(self, text="Submit")
+        self.submit_button = ttk.Button(self, text="Submit", command=self.submit)
+
+        self.confirmation_label = ttk.Label(self, text="")
 
         # data frame layout
         self.data_label.grid(row=0, column=0, columnspan=2, pady=10)
@@ -84,16 +92,28 @@ class DataFrame(ttk.Frame):
         self.plots_label.grid(row=3, column=0)
         self.plots_button.grid(row=3, column=1)
         self.submit_button.grid(row=4, column=0, columnspan=2, pady=10)
+        self.confirmation_label.grid(row=5, column=0, columnspan=2)
 
-    def load_labelled(self):
-        global tweet_dir
-        tweet_dir = filedialog.askopenfilename(initialdir="/home/jake/Documents/twitter-floods-root/twitter-floods/data/StormFranklin")
+    def get_labelled_tweets_dir(self):
+        tweets_dir = filedialog.askopenfilename(initialdir="data/StormFranklin")
+        self.tweets_dir = tweets_dir
 
-    def load_places(self):
-        ...
+    def get_places_dir(self):
+        places_dir = filedialog.askopenfilename(initialdir="data/StormFranklin")
+        self.places_dir = places_dir
 
-    def ask_dir(self):
-        ...
+    def get_plots_dir(self):
+        plots_dir = filedialog.askdirectory(initialdir="/plots")
+        self.plots_dir = plots_dir
+
+    def submit(self):
+        dirs = [self.tweets_dir, self.places_dir, self.plots_dir]
+        if all(i is not None for i in dirs):
+            self.confirmation_label["text"] = "Files Added"
+            self.data_loaded = True
+        else:
+            self.confirmation_label["text"] = "Files Missing"
+            return False
 
 
 class MetaFrame(ttk.Frame):
@@ -167,11 +187,25 @@ class MainWindow(tk.Tk):
 def main():
     root = MainWindow()
 
-    tweets_dir = tk.StringVar()
-    places_dir = tk.StringVar()
-    save_dir = tk.StringVar()
+    tweets_dir = None
+    places_dir = None
+    plots_dir = None
 
-    print(tweet_dir)
+    def get_tweets_dir():
+        return root.options_frame.data_frame.tweets_dir
+
+    def get_places_dir():
+        return root.options_frame.data_frame.places_dir
+
+    def get_plots_dir():
+        return root.options_frame.data_frame.plots_dir
+
+    if root.options_frame.data_frame.data_loaded:
+        tweets_dir = get_tweets_dir()
+        places_dir = get_places_dir()
+        plots_dir = get_plots_dir()
+
+    print(tweets_dir)
 
     root.mainloop()
 
