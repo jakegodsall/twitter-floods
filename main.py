8c2f15717c813fa06e2c24fd17515b54a5859f3f
Jake Godsall <jake.edward.godsall@gmail.com>
2022-08-03 15:06:27 +0100

    Added comments for all classes and methods in the GUI, added Save functionality.

--- main.py
+++ main.py
@@ -1,83 +1,28 @@
-# from twitter_utils.twitter_utils import *
-#
-# def main():
-#     tweets = 'data/StormFranklin/tweets-labelled.jsonl'
-#     places = 'data/StormFranklin/places.jsonl'
-#     save_dir = 'plots'
-#
-#     loader = Loader(tweets, places)
-#
-#     tweets_df, places_df = loader.load_df()
-#
-#     processor = Processor(tweets_df, places_df)
-#
-#     df = processor.extract_features()
-#
-#     print(df.columns)
-#     #
-#     # by_date = processor.create_temporal()
-#     #
-#     # print(by_date.keys())
-#     #
-#     # splot = StaticPlotter()
-#     #
-#     # basemap_plots = splot.plot_basemap(df)
-#     # freq_plot = splot.plot_frequency(by_date)
-#     #
-#     # print(basemap_plots)
-#     #
-#     # tplot = TemporalPlotter()
-#     #
-#     # basemaps = tplot.temporal_plotter(by_date)
-#     # basemaps = tplot.change_structure(basemaps)
-#     #
-#     #
-#     # static_saver = StaticSaver(save_dir, 'franklin')
-#     # temporal_saver = TemporalSaver(save_dir, 'franklin')
-#     # #
-#     # static_saver.create_event_directory()
-#     # static_saver.save_plots(basemap_plots)
-#     # static_saver.save_plots(freq_plot)
-#
-#     # temporal_saver.create_event_directory()
-#     # temporal_saver.save_all_plots(basemaps)
-#
-#     meta = MetaData()
-#     meta_json = meta.generate_json(tweets_df, places_df)
-#
-#     meta_saver = MetaSaver(save_dir, 'franklin')
-#     meta_saver.create_event_directory()
-#
-#     meta_saver.save_meta(meta_json)
-#
-# if __name__ == "__main__":
-#     main()
-
-# gui
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 
-# plotting
-import numpy as np
-import matplotlib as mpl
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
 
 from twitter_utils.twitter_utils import *
 
 
 class DataFrame(ttk.Frame):
+    """
+        This frame represents the first row of the first column of the GUI.
+        The purpose of this frame is to allow the user to set the variables
+        for labelled tweets file, places file and plots directory.
+    """
     def __init__(self, main, *args, **kwargs):
         super().__init__(main, *args, **kwargs)
         # paths
-        self.data_loaded = False
-        self.tweets_dir = None
-        self.places_dir = None
-        self.plots_dir = None
+        self.data_loaded = False  # Boolean of whether the data has been loaded
+        self.tweets_dir = None  # store the tweets directory as a string
+        self.places_dir = None  # store the places directory as a string
+        self.plots_dir = None  # store the plots directory as a string
         self.dirs = tk.StringVar()
 
-        # data frame elements
+        # components
         self.data_label = ttk.Label(self, text="Add Data:")
 
         self.labelled_tweets_label = ttk.Label(self, text="Labelled-Tweets: ")
@@ -105,18 +50,32 @@ class DataFrame(ttk.Frame):
         self.confirmation_label.grid(row=5, column=0, columnspan=2)
 
     def get_labelled_tweets_dir(self):
+        """
+            Return the tweets path from the filedialog to the instance variable.
+        """
         tweets_dir = filedialog.askopenfilename(initialdir="data/StormFranklin")
         self.tweets_dir = tweets_dir
 
     def get_places_dir(self):
+        """
+            Return the places path from the filedialog to the instance variable.
+        """
         places_dir = filedialog.askopenfilename(initialdir="data/StormFranklin")
         self.places_dir = places_dir
 
     def get_plots_dir(self):
+        """
+            Return the plots directory from the filedialog to the instance variable.
+        """
         plots_dir = filedialog.askdirectory(initialdir="/plots")
         self.plots_dir = plots_dir
+        print(self.plots_dir)
 
     def submit(self):
+        """
+            Submit the directories to store in the instance variables.
+            Includes some basic validation.
+        """
         dirs = [self.tweets_dir, self.places_dir, self.plots_dir]
         if all(i is not None for i in dirs):
             self.confirmation_label["text"] = "Files Added"
@@ -127,6 +86,10 @@ class DataFrame(ttk.Frame):
 
 
 class MetaFrame(ttk.Frame):
+    """
+        This frame represents the second row of the first column of the GUI.
+        The purpose of this frame is to show the meta data of the dataset provided.
+    """
     def __init__(self, main, *args, **kwargs):
         super().__init__(main, *args, **kwargs)
 
@@ -143,6 +106,9 @@ class MetaFrame(ttk.Frame):
         self.meta_textbox.grid(row=1, column=0)
 
     def show_meta(self):
+        """
+            Show the meta data in the meta textbox.
+        """
         tweets = self.master.data_frame.tweets_dir
         places = self.master.data_frame.places_dir
 
@@ -156,62 +122,105 @@ class MetaFrame(ttk.Frame):
 
 
 class SaveFrame(ttk.Frame):
+    """
+        This frame represents the final row of the first column of the GUI.
+        The purpose of this frame is provide an interface by which the user can
+        make a plot to be visualised within the GUI, as well as save the plots
+        generated.
+    """
     def __init__(self, main, *args, **kwargs):
+        # super the ttk.Frame object
         super().__init__(main, *args, **kwargs)
 
-        self.event_name = None
-        self.basemap_plots = None
-        self.freq_plot = None
-        self.basemaps = None
-        self.meta_json = None
+        self.event_name = None # event name of directory creation
+        self.static_plots = None  # dictionary of static plots
+        self.freq_plot = None  # variable of frequency plot
+        self.temporal_plots = None  # dictionary of temporal plots
+        self.meta_json = None  # variable for meta data JSON
 
 
         # save frame components
         self.plot_button = ttk.Button(self, text="Plot", command=self.plot)
-        self.save_button = ttk.Button(self, text="Save")
+        self.save_button = ttk.Button(self, text="Save", command=self.save)
 
         # save frame layout
         self.plot_button.grid(row=0, column=0)
         self.save_button.grid(row=0, column=1)
 
     def generate_plots(self):
+        """
+            Generates all plots (static and temporal)
+            for the data set provided. Also generates meta
+            data.
+        """
+        # get the dataset files
         tweets = self.master.data_frame.tweets_dir
         places = self.master.data_frame.places_dir
+
+        # get the event name to create a directory for the event
         self.event_name = tweets.split("/")[-2]
+
+        # load the datasets using Loader to get pandas.DataFrame objects
         loader = Loader(tweets, places)
         tweets_df, places_df = loader.load_df()
+
+        # process the dataframes using Processor
         processor = Processor(tweets_df, places_df)
         df = processor.extract_features()
         by_date = processor.create_temporal()
-        splot = StaticPlotter()
-        self.basemap_plots = splot.plot_basemap(df)
-        self.freq_plot = splot.plot_frequency(by_date)
-        tplot = TemporalPlotter()
-        self.basemaps = tplot.temporal_plotter(by_date)
-        self.basemaps = tplot.change_structure(self.basemaps)
 
+        # plot the static plots using StaticPlotter and store in self.static_plots
+        splot = StaticPlotter()
+        self.static_plots = splot.plot_basemap(df)
+        # plot the frequency plot using StaticPlotter and store in self.freq_plot
+        self.freq_plot = splot.plot_frequency(by_date)
+
+        # plot the temporal plots using TemporalPlotter and store in self.temporal_plots
+        tplot = TemporalPlotter()
+        self.temporal_plots = tplot.temporal_plotter(by_date)
+        self.temporal_plots = tplot.change_structure(self.temporal_plots)
+
+        # get the meta data using MetaData and store in self.meta_json
         meta = MetaData(tweets_df, places_df)
         self.meta_json = meta.generate_json()
 
     def plot(self):
+        """
+            Plot the static plot generated by self.generate_plots()
+            on the TkCanvas in the PlotFrame.
+        """
         self.generate_plots()
         self.master.master.plot_frame.plot()
 
     def save(self):
-        save_dir = self.master.data_frame.save_dir
+        """
+            Saves the static plots, temporal plots and meta data to disk.
+        """
+        # get the save directory
+        save_dir = self.master.data_frame.plots_dir
+
+        # save the static plots
         static_saver = StaticSaver(save_dir, self.event_name)
-        temporal_saver = TemporalSaver(save_dir, self.event_name)
         static_saver.create_event_directory()
-        static_saver.save_plots(self.basemap_plots)
+        static_saver.save_plots(self.static_plots)
         static_saver.save_plots(self.freq_plot)
+
+        # save the temporal plots
+        temporal_saver = TemporalSaver(save_dir, self.event_name)
         temporal_saver.create_event_directory()
-        temporal_saver.save_all_plots(self.basemaps)
+        temporal_saver.save_all_plots(self.temporal_plots)
+
+        # save the meta data
         meta_saver = MetaSaver(save_dir, 'franklin')
         meta_saver.create_event_directory()
         meta_saver.save_meta(self.meta_json)
 
 
 class OptionsFrame(ttk.Frame):
+    """
+        This is the first column of the GUI, which
+        includes the DataFrame, the MetaFrame and the SaveFrame.
+    """
     def __init__(self, main, *args, **kwargs):
         super().__init__(main, *args, **kwargs)
         # options frame components
@@ -230,11 +239,18 @@ class OptionsFrame(ttk.Frame):
 
 
 class PlotFrame(ttk.Frame):
+    """
+        This is the second column of the GUI where the plot will
+        be displayed.
+    """
     def __init__(self, main, *args, **kwargs):
         super().__init__(main, *args, **kwargs)
 
     def plot(self):
-        fig = self.master.options_frame.save_frame.basemap_plots['density']
+        """
+            Render the plot generated earlier to the Tk Canvas.
+        """
+        fig = self.master.options_frame.save_frame.static_plots['density']
 
         canvas = FigureCanvasTkAgg(fig, master=self)
         canvas.draw()
